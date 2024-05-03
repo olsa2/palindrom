fd038c1ab7dbf1bf071695bc2815e70623b610e9
Oleksiy Sarkhovskyy <o.sarkhovskyy@gmail.com>
2024-05-03 08:17:03 +0200

    Resources has been added

--- src/de/htwsaar/sarkhovska/palindrom/PalindromTestDrive.java
+++ src/de/htwsaar/sarkhovska/palindrom/PalindromTestDrive.java
@@ -5,10 +5,19 @@ import java.util.List;
 import java.util.Arrays;
 
 public class PalindromTestDrive {
+    // static instance of PalindromIterative accessible global
     static Palindrom paliIterative = new PalindromIterative();
+
+    // static instance of PalindromRecursive accessible global
     static Palindrom paliRecursive = new PalindromRecursive();
+
+    //static StringBuffer to store comma separated lengths of examined words
     static StringBuffer bufferLen = new StringBuffer();
+
+    //static StringBuffer to store runtime measurement for the iterative implementation
     static StringBuffer bufferIterative = new StringBuffer();
+
+    //static StringBuffer to store runtime measurement for the recursive implementation
     static StringBuffer bufferRecursive = new StringBuffer();
 
     public static void main(String[] args) {
@@ -22,7 +31,7 @@ public class PalindromTestDrive {
 
         List<String> argList = Arrays.asList(args);
 
-        if (argList.get(0).equals("-f")) {
+        if (argList.get(0).equals("-f")) { // -f flag was specified at the first place in the arguments, read words from the resource file words.txt
             File file = new File(loader.getResource("words.txt").getFile());
             try (BufferedReader bf = new BufferedReader(new FileReader(file))) {
                 String word = null;
@@ -37,12 +46,13 @@ public class PalindromTestDrive {
                 System.exit(4);
             }
 
-        } else {
+        } else { //words are provided inline in the command string
             for (String word : argList) {
                 doMeasurement(word);
             }
         }
 
+        // save measurements to the resource file measurement.txt
         File file = new File(loader.getResource("measurement.txt").getFile());
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
             bw.append(bufferLen.toString());
@@ -77,14 +87,14 @@ public class PalindromTestDrive {
         }
         bufferIterative.append(end - start);
 
-        start = System.nanoTime();
-        paliRecursive.isPalindrom(word);
-        end = System.nanoTime();
+        start = System.nanoTime(); //get the start time
+        paliRecursive.isPalindrom(word); //run the method to be measured
+        end = System.nanoTime(); //get the end time
 
         if (!bufferRecursive.isEmpty()) {
             bufferRecursive.append(",");
         }
-        bufferRecursive.append(end - start);
+        bufferRecursive.append(end - start); //write out the runtime interval
 
         System.out.println(word + " = " + bPalindrom);
     }
